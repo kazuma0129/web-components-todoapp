e20540260fc2c6dd562509b39bac640bc7350939
KazumaOhashi <tikuwaaaaaaaa@gmail.com>
2021-11-02 20:21:07 +0900

    Fix: by linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  mode: "none",
-  entry: "./src/app.js",
+  mode: 'none',
+  entry: './src/app.js',
   output: {
-    path: __dirname + "/dist",
-    filename: "app.js",
+    path: __dirname + '/dist',
+    filename: 'app.js',
   },
   devServer: {
     static: {
-      directory: require("path").join(__dirname, "dist"),
+      directory: require('path').join(__dirname, 'dist'),
     },
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },
