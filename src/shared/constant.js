7a9835bcb3ea75bc23788b3fe3791fd341d4c7e0
KazumaOhashi <tikuwaaaaaaaa@gmail.com>
2021-11-03 19:30:08 +0900

    Update: constants of default values

--- src/shared/constant.js
+++ src/shared/constant.js
@@ -2,3 +2,6 @@ export const STORE_KEY_DELIMITER = ':';
 export const STORE_KEY_PREFIX_DEFAULT = 'papyrus-todoapp';
 export const STORE_KEY_PREFIX_TODO = `${STORE_KEY_PREFIX_DEFAULT}${STORE_KEY_DELIMITER}todo`;
 export const STORE_KEY_PREFIX_CATEGORY = `${STORE_KEY_PREFIX_DEFAULT}${STORE_KEY_DELIMITER}category`;
+
+export const CATEGORIES_DEFAULT = ['Work', 'Idea', 'Shop'];
+export const TODO_STATUS_DEFAULT = ['all', 'yet', 'done'];
